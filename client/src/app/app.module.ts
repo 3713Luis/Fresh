b93a36bff77711ddac55670c0e779f9d3221346b
3713Luis <Antoniorc3713@gmail.com>
2019-03-11 17:10:43 -0600

    Se inserto menú y slider

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    RegistrarUsuarioComponent,
+    SliderComponent,
+    
   ],
   imports: [
     BrowserModule,
