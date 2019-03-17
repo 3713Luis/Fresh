482bcd0a8cad69f820862184f427de675fd4c945
3713Luis <Antoniorc3713@gmail.com>
2019-03-16 19:37:31 -0600

    Enrutamiento de componentes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SliderComponent} from  './components/slider/slider.component'
+import {LoginComponent } from './components/login/login.component'
+import { RegistrarUsuarioComponent } from  './components/registrar-usuario/registrar-usuario.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+path: '',
+redirectTo: '/slider',
+pathMatch: 'full'
+  },
+  {
+    path: 'slider',
+    component: SliderComponent
+  },
+
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'registro',
+    component: RegistrarUsuarioComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
